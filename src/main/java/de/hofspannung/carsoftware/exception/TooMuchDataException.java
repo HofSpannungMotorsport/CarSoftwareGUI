abde3e817a737c78f056a492f3317628632a5374
iWald <andreas.waldhuetter@hof-university.de>
2019-12-18 12:28:04 +0100

    Added TooMuchDataException

---
+++ src/main/java/de/hofspannung/carsoftware/exception/TooMuchDataException.java
@@ -0,0 +1,38 @@
+package de.hofspannung.carsoftware.exception;
+
+public class TooMuchDataException extends Exception {
+
+    boolean hasLength;
+    long length;
+    long maxLength;
+
+    public TooMuchDataException() {
+        super();
+    }
+
+    public TooMuchDataException(String message) {
+        super(message);
+    }
+
+    public TooMuchDataException(long length, long maxLength) {
+        super();
+        this.length = length;
+        this.maxLength = maxLength;
+        this.hasLength = true;
+    }
+
+    public TooMuchDataException(String message, long length, long maxLength) {
+        this(message);
+        this.length = length;
+        this.maxLength = maxLength;
+        this.hasLength = true;
+    }
+
+    @Override
+    public String toString() {
+        String s = super.toString();
+        if (hasLength)
+            s += "\nAdditional INFO:   length: " + length + "   maxLength: " + maxLength;
+        return s;
+    }
+}

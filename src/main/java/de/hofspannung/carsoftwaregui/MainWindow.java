71f476af1fb3b28606ead68b5ba887c8e72b76c5
iWald <awaldhuetter@hof-university.de>
2021-02-12 23:23:16 +0100

    Implemented Port selection

--- src/main/java/de/hofspannung/carsoftwaregui/MainWindow.java
+++ src/main/java/de/hofspannung/carsoftwaregui/MainWindow.java
@@ -1,11 +1,33 @@
 package de.hofspannung.carsoftwaregui;
 
+import com.fazecast.jSerialComm.SerialPort;
+import de.hofspannung.carsoftware.registry.RegistryController;
+import de.hofspannung.carsoftwaregui.controls.RegistryTab;
 import java.io.IOException;
+import java.util.Collection;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
+import javafx.util.StringConverter;
+import org.jetbrains.annotations.NotNull;
 
 public class MainWindow extends BorderPane {
+
+  @FXML
+  private TabPane registries;
+
+  @FXML
+  private ChoiceBox<SerialPort> ports;
+
+  @FXML
+  private Button connect;
+  @FXML
+  private Button disconnect;
+
+  private RegistryController controller;
 
   public MainWindow() {
     var loader = new FXMLLoader(getClass().getResource("mainwindow.fxml"));
@@ -17,5 +39,36 @@ public class MainWindow extends BorderPane {
     } catch (IOException e) {
       e.printStackTrace();
     }
+
+    ports.setConverter(
+        new StringConverter<>() {
+          @Override
+          public String toString(SerialPort serialPort) {
+            if (serialPort == null) {
+              return "----";
+            }
+            return serialPort.getDescriptivePortName();
+          }
+
+          @Override
+          public SerialPort fromString(String s) {
+            return null;
+          }
+        });
+  }
+
+  public void setPorts(Collection<SerialPort> ports) {
+    this.ports.getItems().clear();
+    this.ports.getItems().addAll(ports);
+  }
+
+  public void setController(@NotNull RegistryController controller) {
+    registries.getTabs().clear();
+
+    this.controller = controller;
+
+    controller.getTypes().forEach(registryType -> {
+      registries.getTabs().add(new RegistryTab(controller.getRegistry(registryType)));
+    });
   }
 }
--- src/main/java/de/hofspannung/carsoftwaregui/main/App.java
+++ src/main/java/de/hofspannung/carsoftwaregui/main/App.java
@@ -1,6 +1,8 @@
 package de.hofspannung.carsoftwaregui.main;
 
+import com.fazecast.jSerialComm.SerialPort;
 import de.hofspannung.carsoftwaregui.MainWindow;
+import java.util.Arrays;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -14,8 +16,16 @@ public class App extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     primaryStage.setTitle("CarSoftware GUI - HofSpannung Motorsport e.V.");
-    var scene = new Scene(new MainWindow(), 600, 400);
+    var main = new MainWindow();
+    var scene = new Scene(main, 600, 400);
     primaryStage.setScene(scene);
     primaryStage.show();
+
+    main.setPorts(Arrays.asList(SerialPort.getCommPorts()));
+
+    // load registries
+
+    // link registries with connection
+    // through a new controller?
   }
 }
